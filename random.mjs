6eca481fd4af88305244d9611586c465796e98e7
Arya Karimi <devaryakrm@gmail.com>
2022-12-19 18:06:26 +0300

    code added

--- random.mjs
+++
@@ -1,6 +0,0 @@
-import { randomBytes } from "node:crypto";
-
-randomBytes(256, (err, buf) => {
-  if (err) throw err;
-  console.log(`${buf.length} bytes of random data: ${buf.toString("hex")}`);
-});
---
+++ use.js
@@ -0,0 +1,3 @@
+const sum = require("./sum");
+
+sum(5, 7);
