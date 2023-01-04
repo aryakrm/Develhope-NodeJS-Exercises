06c8a42b451b15426b6e3ad3e618a793cb79ccb5
Arya Karimi <devaryakrm@gmail.com>
2023-01-04 22:42:13 +0300

    Prisma Model Done

---
+++ prisma/migrations/20230104193550_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "age" INTEGER NOT NULL,
+    "status" VARCHAR(255),
+    "isPassed" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
